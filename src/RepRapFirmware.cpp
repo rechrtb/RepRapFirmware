dff6695c438fe9a940ca0eaf9553a3aa1c9e1350
David Crocker <dcrocker@eschertech.com>
2024-11-27 19:51:22 +0000

    Use 'g' format in GetFloatFormatString so we don't lose significant digs

--- src/RepRapFirmware.cpp
+++ src/RepRapFirmware.cpp
@@ -167,7 +167,7 @@ RepRap reprap;
 // Get the format string to use for printing a floating point number to the specified number of decimal digits. Zero means the maximum sensible number.
 const char *_ecv_array GetFloatFormatString(float val, unsigned int numDigitsAfterPoint) noexcept
 {
-	static constexpr const char *_ecv_array FormatStrings[] = { "%.7f", "%.1f", "%.2f", "%.3f", "%.4f", "%.5f", "%.6f", "%.7f" };
+	static constexpr const char *_ecv_array FormatStrings[] = { "%.7g", "%.1g", "%.2g", "%.3g", "%.4g", "%.5g", "%.6g", "%.7g" };
 	static_assert(ARRAY_SIZE(FormatStrings) == MaxFloatDigitsDisplayedAfterPoint + 1);
 
 	float f = 1.0;
