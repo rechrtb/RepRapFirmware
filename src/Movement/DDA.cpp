rechrtb/RepRapFirmware

commit da0c1ceb33769bae055aab871552e90b96f6b49b

Fixed issue 1064

src/Movement/DDA.cpp
@@ -223,6 +223,7 @@ void DDA::DebugPrint(const char *tag) const noexcept
 bool DDA::InitStandardMove(DDARing& ring, const RawMove &nextMove, bool doMotorMapping) noexcept
 {
 	// 0. If there are more total axes than visible axes, then we must ignore any movement data in nextMove for the invisible axes.
+	// Likewise we must ignore any movement data in nextMove for unowned axes.
 	// The call to CartesianToMotorSteps may adjust the invisible axis endpoints for architectures such as CoreXYU and delta with >3 towers, so set them up here.
 	const size_t numTotalAxes = reprap.GetGCodes().GetTotalAxes();
 	const size_t numVisibleAxes = reprap.GetGCodes().GetVisibleAxes();
@@ -256,47 +257,66 @@ bool DDA::InitStandardMove(DDARing& ring, const RawMove &nextMove, bool doMotorM
 
 		if (drive < numVisibleAxes)
 		{
-			if (doMotorMapping)
+#if SUPPORT_ASYNC_MOVES
+			if (nextMove.axesAndExtrudersOwned.IsBitSet(drive))
+#endif
 			{
-				endCoordinates[drive] = nextMove.coords[drive];
-				const float positionDelta = endCoordinates[drive] - prev->GetEndCoordinate(drive, false);
-				directionVector[drive] = positionDelta;
-				if (positionDelta != 0.0)
+				if (doMotorMapping)
 				{
-					if (reprap.GetMove().IsAxisRotational(drive) && nextMove.rotationalAxesMentioned)
+					endCoordinates[drive] = nextMove.coords[drive];
+					const float positionDelta = endCoordinates[drive] - prev->GetEndCoordinate(drive, false);
+					directionVector[drive] = positionDelta;
+					if (positionDelta != 0.0)
 					{
-						rotationalAxesMoving = true;
+						if (reprap.GetMove().IsAxisRotational(drive) && nextMove.rotationalAxesMentioned)
+						{
+							rotationalAxesMoving = true;
+						}
+						else if (nextMove.linearAxesMentioned)
+						{
+							linearAxesMoving = true;
+						}
+						if (Tool::GetXAxes(nextMove.movementTool).IsBitSet(drive) || Tool::GetYAxes(nextMove.movementTool).IsBitSet(drive))
+						{
+							flags.xyMoving = true;				// this move has XY movement in user space, before axis were mapped
+						}
 					}
-					else if (nextMove.linearAxesMentioned)
+				}
+				else
+				{
+					// Raw motor move on a visible axis
+					endPoint[drive] = move.MotorMovementToSteps(drive, nextMove.coords[drive]);
+					const int32_t delta = endPoint[drive] - positionNow[drive];
+					directionVector[drive] = (float)delta/move.DriveStepsPerMm(drive);
+					if (delta != 0)
 					{
-						linearAxesMoving = true;
-					}
-					if (Tool::GetXAxes(nextMove.movementTool).IsBitSet(drive) || Tool::GetYAxes(nextMove.movementTool).IsBitSet(drive))
-					{
-						flags.xyMoving = true;				// this move has XY movement in user space, before axis were mapped
+						if (reprap.GetMove().IsAxisRotational(drive))
+						{
+							rotationalAxesMoving = true;
+						}
+						else
+						{
+							linearAxesMoving = true;
+						}
 					}
 				}
 			}
+#if SUPPORT_ASYNC_MOVES
 			else
 			{
-				// Raw motor move on a visible axis
-				endPoint[drive] = move.MotorMovementToSteps(drive, nextMove.coords[drive]);
-				const int32_t delta = endPoint[drive] - positionNow[drive];
-				directionVector[drive] = (float)delta/move.DriveStepsPerMm(drive);
-				if (delta != 0)
-				{
-					if (reprap.GetMove().IsAxisRotational(drive))
-					{
-						rotationalAxesMoving = true;
-					}
-					else
-					{
-						linearAxesMoving = true;
-					}
-				}
+				// This is an axis we don't own, so make sure we don't move it
+				directionVector[drive] = 0.0;
+				prev->endCoordinates[drive] = nextMove.coords[drive];
+				endPoint[drive] = positionNow[drive];
 			}
+#endif
 		}
-		else if (LogicalDriveToExtruder(drive) < reprap.GetGCodes().GetNumExtruders())
+		else if (
+#if SUPPORT_ASYNC_MOVES
+					nextMove.axesAndExtrudersOwned.IsBitSet(drive) &&
+#endif
+					LogicalDriveToExtruder(drive) < reprap.GetGCodes().GetNumExtruders()
+				)
 		{
 			// It's an extruder drive. We defer calculating the steps because they may be affected by nonlinear extrusion, which we can't calculate until we
 			// know the speed of the move, and because extruder movement is relative so we need to accumulate fractions of a whole step between moves.

src/Version.h
@@ -10,7 +10,7 @@
 
 #ifndef VERSION
 // Note: the complete VERSION string must be in standard version number format and must not contain spaces! This is so that DWC can parse it.
-# define MAIN_VERSION	"3.6.0-beta.2+2"
+# define MAIN_VERSION	"3.6.0-beta.2+3"
 # ifdef USE_CAN0
 #  define VERSION_SUFFIX	"(CAN0)"
 # else

src/GCodes/GCodes.cpp
@@ -3063,7 +3063,9 @@ bool GCodes::ReadMove(MovementSystemNumber queueNumber, RawMove& m) noexcept
 				}
 				axisMap0 = Tool::GetAxisMapping(ms.movementTool, ms.arcAxis0);
 				axisMap1 = Tool::GetAxisMapping(ms.movementTool, ms.arcAxis1);
+#if 0	// we don't use this yet
 				ms.cosXyAngle = (ms.xyPlane) ? ms.angleIncrementCosine : 1.0;
+#endif
 			}
 
 			for (size_t drive = 0; drive < numVisibleAxes; ++drive)

src/Movement/RawMove.cpp
@@ -38,7 +38,9 @@ void MovementState::SetDefaults(size_t firstDriveToZero) noexcept
 	filePos = noFilePosition;
 	movementTool = nullptr;
 	moveFractionToSkip = 0.0;
+#if 0	// we don't use this yet
 	cosXyAngle = 1.0;
+#endif
 	for (size_t drive = firstDriveToZero; drive < MaxAxesPlusExtruders; ++drive)
 	{
 		coords[drive] = 0.0;					// clear extrusion

src/Movement/RawMove.h
@@ -20,11 +20,17 @@ struct RawMove
 	float moveStartVirtualExtruderPosition;							// the virtual extruder position at the start of this move, for normal moves
 	FilePosition filePos;											// offset in the file being printed at the start of reading this move
 	float proportionDone;											// what proportion of the entire move has been done when this segment is complete
+#if 0	// we don't use this yet
 	float cosXyAngle;												// the cosine of the change in XY angle between the previous move and this move
+#endif
 	float maxPrintingAcceleration;
 	float maxTravelAcceleration;
 
 	const Tool *_ecv_null movementTool;								// which tool (if any) is being used by this move
+
+#if SUPPORT_ASYNC_MOVES
+	AxesBitmap axesAndExtrudersOwned;								// axes and extruders that this movement system has moved since the last sync, or owns for other reasons
+#endif
 
 	uint16_t moveType : 3,											// the H parameter from the G0 or G1 command, 0 for a normal move
 			applyM220M221 : 1,										// true if this move is affected by M220 and M221 (this could be moved to ExtendedRawMove)
@@ -210,7 +216,6 @@ private:
 	MovementSystemNumber msNumber;
 
 #if SUPPORT_ASYNC_MOVES
-	AxesBitmap axesAndExtrudersOwned;								// axes and extruders that this movement system has moved since the last sync
 	ParameterLettersBitmap ownedAxisLetters;						// cache of letters denoting user axes for which the corresponding machine axes for the current tool are definitely owned
 
 	static AxesBitmap axesAndExtrudersMoved;						// axes and extruders that are owned by any movement system

commit 27d956ab5578a36c7698a3adb01acc4e79c03a18

Fixed CoreXY kinematics broken in 3.6.0-beta.2_3, this is beta.2+4
Was broken by commit da0c1ceb33769bae055aab871552e90b96f6b49b

src/Movement/DDA.cpp
@@ -227,6 +227,21 @@ bool DDA::InitStandardMove(DDARing& ring, const RawMove &nextMove, bool doMotorM
 	// The call to CartesianToMotorSteps may adjust the invisible axis endpoints for architectures such as CoreXYU and delta with >3 towers, so set them up here.
 	const size_t numTotalAxes = reprap.GetGCodes().GetTotalAxes();
 	const size_t numVisibleAxes = reprap.GetGCodes().GetVisibleAxes();
+	const Move& move = reprap.GetMove();
+
+#if SUPPORT_ASYNC_MOVES
+	DriversBitmap ownedDrivers;
+	const Kinematics& kin = move.GetKinematics();
+	for (size_t axis = 0; axis < numVisibleAxes; ++axis)
+	{
+		if (nextMove.axesAndExtrudersOwned.IsBitSet(axis))
+		{
+			ownedDrivers |= kin.GetControllingDrives(axis, false);
+		}
+	}
+#endif
+
+	// Set any invisible axis endpoints to the same positions as the previous move
 	const int32_t * const positionNow = prev->DriveCoordinates();
 	for (size_t axis = numVisibleAxes; axis < numTotalAxes; ++axis)
 	{
@@ -236,7 +251,6 @@ bool DDA::InitStandardMove(DDARing& ring, const RawMove &nextMove, bool doMotorM
 	flags.all = 0;														// set all flags false
 
 	// 1. Compute the new endpoints and the movement vector
-	const Move& move = reprap.GetMove();
 	if (doMotorMapping)
 	{
 		if (!move.CartesianToMotorSteps(nextMove.coords, endPoint, nextMove.isCoordinated))		// transform the axis coordinates if on a delta or CoreXY printer
@@ -258,7 +272,7 @@ bool DDA::InitStandardMove(DDARing& ring, const RawMove &nextMove, bool doMotorM
 		if (drive < numVisibleAxes)
 		{
 #if SUPPORT_ASYNC_MOVES
-			if (nextMove.axesAndExtrudersOwned.IsBitSet(drive))
+			if (ownedDrivers.IsBitSet(drive))
 #endif
 			{
 				if (doMotorMapping)

src/Version.h
@@ -10,7 +10,7 @@
 
 #ifndef VERSION
 // Note: the complete VERSION string must be in standard version number format and must not contain spaces! This is so that DWC can parse it.
-# define MAIN_VERSION	"3.6.0-beta.2+3"
+# define MAIN_VERSION	"3.6.0-beta.2+4"
 # ifdef USE_CAN0
 #  define VERSION_SUFFIX	"(CAN0)"
 # else